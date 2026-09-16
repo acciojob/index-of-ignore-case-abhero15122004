fb70252109e683649783303abe8d749ff8add75b
Abhinay Reddy <149326091+abhero15122004@users.noreply.github.com>
2026-09-16 17:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function indexOfIgnoreCase(s1, s2) {
   if (s1 === null || s1 === undefined || s2 === null || s2 === undefined) {
     return -1;
   }
-  return str.toLowerCase().indexOf(subStr.toLowerCase());
+  return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
